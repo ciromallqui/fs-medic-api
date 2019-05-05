88094b438d31abb3784d42e0e20bd8d70cb80c09
Ciro Mallqui <cmallqui@expertsystem.pe>
2019-05-05 11:51:45 -0500

    first change of project

---
+++ src/main/java/medic/pe/fsmedicapi/FsMedicApiApplication.java
@@ -0,0 +1,13 @@
+package medic.pe.fsmedicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsMedicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsMedicApiApplication.class, args);
+	}
+
+}
